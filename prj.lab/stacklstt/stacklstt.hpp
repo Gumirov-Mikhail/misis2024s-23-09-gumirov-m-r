762da0f0de067743176c6330bb6c0fac60e2844d
Gumirov-Mikhail <m2309426@edu.misis.ru>
2024-04-14 01:40:17 +0300

    upd: stacklstt

--- prj.lab/stacklstt/stacklstt.hpp
+++ prj.lab/stacklstt/stacklstt.hpp
@@ -2,7 +2,10 @@
 #ifndef STACKLSTT_STACKLSTT_HPP_202404
 #define STACKLSTT_STACKLSTT_HPP_202404
 
+#include <algorithm>
+#include <stdexcept>
 #include <cstddef>
+#include <initializer_list>
 
 template <typename T>
 class StackLstT {
@@ -36,5 +39,164 @@ private:
 
     Node* head_ = nullptr;
 };
+
+template<typename T>
+StackLstT<T>::StackLstT() = default;
+
+template<typename T>
+StackLstT<T>::~StackLstT() {
+    Node* temp = head_;
+    while (temp != nullptr) {
+        head_ = head_->next;
+        delete temp;
+        temp = head_;
+    }
+}
+
+template<typename T>
+StackLstT<T>::StackLstT(const StackLstT<T> &other) {
+    if (!other.empty()) {
+        Node* temp = new Node {other.head_->value, nullptr};
+        head_ = temp;
+        Node* p_src = other.head_->next;
+        Node* p_cur = temp;
+        while (p_src != nullptr) {
+            temp = new Node {p_src->value, nullptr};
+            p_cur->next = temp;
+            p_src = p_src->next;
+            p_cur = temp;
+        }
+    }
+}
+
+template<typename T>
+StackLstT<T>::StackLstT(StackLstT<T> &&other) {
+    this->swap(other);
+}
+
+template<typename T>
+StackLstT<T>::StackLstT(const std::initializer_list<T> &list) {
+    T* mas = new T [list.size()];
+    std::copy(list.end(), list.begin(), mas);
+    Node* temp = new Node {mas[0], nullptr};
+    head_ = temp;
+    Node* p_cur = temp;
+    for (int i = 1; i < list.size(); i++) {
+        Node* temp = new Node {mas[i], nullptr};
+        p_cur->next = temp;
+        p_cur = temp;
+    }
+}
+
+template<typename T>
+void StackLstT<T>::push(const T &value) {
+    Node* temp = new Node {value, head_};
+    head_ = temp;
+}
+
+template<typename T>
+void StackLstT<T>::pop() {
+    if (empty()) {
+        throw std::logic_error("StackLstT - try pop from empty stack.");
+    }
+    else {
+        Node* temp = head_;
+        head_ = head_->next;
+        delete temp;
+        temp = nullptr;
+    }
+}
+
+template<typename T>
+T &StackLstT<T>::top() const {
+    if (empty()) {
+        throw std::logic_error("StackLstT - try get top from empty stack.");
+    }
+    return head_->value;
+}
+
+template<typename T>
+void StackLstT<T>::swap(StackLstT<T> &other) {
+    std::swap(head_, other.head_);
+}
+
+template<typename T>
+void StackLstT<T>::merge(StackLstT<T> &other) {
+    if (other.empty()) {
+        this->swap(other);
+    }
+    else {
+        Node* temp = head_;
+        while (temp->next != nullptr) {
+            head_ = head_->next;
+        }
+        temp->next = other.head_;
+    }
+}
+
+template<typename T>
+bool StackLstT<T>::empty() const {
+    if (head_ == nullptr) {
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+std::ptrdiff_t StackLstT<T>::size() const {
+    Node* temp = head_;
+    std::ptrdiff_t counter = 0;
+    while (temp != nullptr) {
+        temp = temp->next;
+        counter++;
+    }
+    return counter;
+}
+
+template<typename T>
+bool StackLstT<T>::operator==(const StackLstT<T> &rhs) const {
+    Node* temp1 = head_;
+    Node* temp2 = rhs.head_;
+    if (size() == rhs.size()) {
+        while (temp2 != nullptr) {
+            if (temp1->value != temp2->value) {
+                return false;
+            }
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool StackLstT<T>::operator!=(const StackLstT<T> &rhs) const {
+    return !operator==(rhs);
+}
+
+template<typename T>
+StackLstT<T> &StackLstT<T>::operator=(const StackLstT<T> &rhs) noexcept {
+    if (this != &rhs) {
+        if (!rhs.empty()) {
+            Node* temp = new Node {rhs.head_->value, nullptr};
+            head_ = temp;
+            Node* p_src = rhs.head_->next;
+            Node* p_cur = temp;
+            while (p_src != nullptr) {
+                temp = new Node {p_src->value, nullptr};
+                p_cur->next = temp;
+                p_src = p_src->next;
+                p_cur = temp;
+            }
+        }
+    }
+}
+
+template<typename T>
+StackLstT<T> &StackLstT<T>::operator=(StackLstT<T> &&other) {
+    this->swap(other);
+    return *this;
+}
 
 #endif
--- prj.test/test_stacklstt.cpp
+++ prj.test/test_stacklstt.cpp
@@ -1,3 +1,203 @@
-//
-// Created by Михаил on 11.04.2024.
-//
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+
+#include <stacklstt/stacklstt.hpp>
+
+TEST_CASE_TEMPLATE("StackLstT - Constructors", T, int, float, double, long long) {
+    SUBCASE("Default constructor") {
+        StackLstT<T> ctor;
+        CHECK(ctor.empty() == true);
+        CHECK(ctor.size() == 0);
+    }
+    SUBCASE("Constructor copy") {
+        StackLstT<T> ctor_copy1;
+        ctor_copy1.push(1);
+        ctor_copy1.push(5);
+        StackLstT<T> ctor_copy2(ctor_copy1);
+        CHECK(ctor_copy2.empty() == false);
+        CHECK(ctor_copy2.top() == 5);
+        CHECK(ctor_copy2.size() == 2);
+    }
+    SUBCASE("Constructor move") {
+        StackLstT<T> ctor_move1;
+        ctor_move1.push(1);
+        ctor_move1.push(5);
+        StackLstT<T> ctor_move2(std::move(ctor_move1));
+        CHECK(ctor_move1.empty() == true);
+        CHECK(ctor_move2.empty() == false);
+        CHECK(ctor_move2.top() == 5);
+        CHECK(ctor_move2.size() == 2);
+    }
+    SUBCASE("Initializer list") {
+        StackLstT<T> init_list {1, 2, 3, 4};
+        CHECK(init_list.empty() == false);
+        CHECK(init_list.top() == 4);
+        CHECK(init_list.size() == 4);
+    }
+    SUBCASE("Constructor copy with empty stack") {
+        StackLstT<T> ctor_copy1;
+        StackLstT<T> ctor_copy2(ctor_copy1);
+        CHECK(ctor_copy1.empty() == true);
+        CHECK(ctor_copy2.empty() == true);
+        CHECK(ctor_copy2.size() == 0);
+    }
+    SUBCASE("Constructor move with empty stack") {
+        StackLstT<T> ctor_move1;
+        StackLstT<T> ctor_move2(std::move(ctor_move1));
+        CHECK(ctor_move1.empty() == true);
+        CHECK(ctor_move2.empty() == true);
+        CHECK(ctor_move1.size() == 0);
+        CHECK(ctor_move2.size() == 0);
+    }
+}
+
+TEST_CASE_TEMPLATE("StackLstT - Push, Pop and Top", T, int, float, double, long long) {
+    SUBCASE("Push and Pop") {
+        StackLstT<T> st1;
+        CHECK(st1.empty() == true);
+        CHECK(st1.size() == 0);
+        st1.push(5);
+        CHECK(st1.top() == 5);
+        CHECK(st1.size() == 1);
+        st1.push(4);
+        CHECK(st1.top() == 4);
+        CHECK(st1.size() == 2);
+        st1.pop();
+        CHECK(st1.top() == 5);
+        CHECK(st1.size() == 1);
+        st1.pop();
+        CHECK(st1.empty() == true);
+        CHECK(st1.size() == 0);
+    }
+    SUBCASE("Pop and Top with empty stack") {
+        StackLstT<T> st1;
+        CHECK_THROWS_WITH(st1.pop(), "StackLstT - try pop from empty stack.");
+        CHECK_THROWS_WITH(st1.top(), "StackLstT - try get top from empty stack.");
+        st1.push(5);
+        CHECK(st1.top() == 5);
+        CHECK(st1.size() == 1);
+        st1.pop();
+        CHECK_THROWS_WITH(st1.pop(), "StackLstT - try pop from empty stack.");
+        CHECK_THROWS_WITH(st1.top(), "StackLstT - try get top from empty stack.");
+    }
+}
+
+TEST_CASE_TEMPLATE("StackLstT - Swap and Merge", T, int, float, double, long long) {
+    SUBCASE("Swap") {
+        StackLstT<T> st1 {5, 4, 3};
+        StackLstT<T> st2 {5, 4, 3};
+        StackLstT<T> st3;
+        StackLstT<T> st4 {4, 3, 2};
+        st1.swap(st3);
+        CHECK((st3 == st2) == true);
+        CHECK(st1.empty() == true);
+        st2.swap(st4);
+        CHECK((st3 == st4) == true);
+        CHECK((st2 != st3) == true);
+    }
+    SUBCASE("Merge") {
+        StackLstT<T> st1 {5, 4, 3};
+        StackLstT<T> st2 {6, 7, 8};
+        StackLstT<T> st_merge {5, 4, 3, 6, 7, 8};
+        st1.merge(st2);
+        CHECK((st1 == st_merge) == true);
+
+        StackLstT<T> st3 {5};
+        StackLstT<T> st4 {4, 3, 6, 7, 8};
+        st3.merge(st4);
+        CHECK((st3 == st_merge) == true);
+
+        StackLstT<T> st5 {5, 4, 3, 6, 7};
+        StackLstT<T> st6 {8};
+        st5.merge(st6);
+        CHECK((st5 == st_merge) == true);
+    }
+    SUBCASE("Merge with empty stack") {
+
+    }
+}
+
+TEST_CASE_TEMPLATE("StackLstT - Compare", T, int, float, double, long long) {
+    SUBCASE("Compare with not empty stack") {
+        StackLstT<T> st1 {5, 4, 3};
+        StackLstT<T> st2 {5, 4, 3};
+        StackLstT<T> st3 {5, 4, 1};
+        CHECK((st1 == st2) == true);
+        CHECK((st1 != st3) == true);
+        CHECK((st2 != st3) == true);
+        CHECK((st1 != st2) == false);
+        CHECK((st1 == st3) == false);
+        CHECK((st2 == st3) == false);
+    }
+    SUBCASE("Compare with empty stack") {
+        StackLstT<T> st1 {5, 4, 3};
+        StackLstT<T> st2;
+        StackLstT<T> st3;
+        CHECK((st1 != st2) == true);
+        CHECK((st1 != st3) == true);
+        CHECK((st2 == st3) == true);
+        CHECK((st1 == st2) == false);
+        CHECK((st1 == st3) == false);
+        CHECK((st2 != st3) == false);
+    }
+}
+
+TEST_CASE_TEMPLATE("StackLstT - Assignment", T, int, float, double, long long) {
+    SUBCASE("Assignment with copy") {
+        StackLstT<T> st1 {5, 4, 3};
+        StackLstT<T> st2;
+        st2 = st1;
+        CHECK(st1.empty() == false);
+        CHECK(st2.empty() == false);
+        CHECK(st2.size() == 3);
+        CHECK(st2.top() == 3);
+        st2.pop();
+        CHECK(st2.top() == 4);
+        CHECK(st2.size() == 2);
+        CHECK(st1.size() == 3);
+        CHECK(st1.top() == 3);
+        st2.pop();
+        st2.pop();
+        CHECK(st1.empty() == false);
+        CHECK(st2.empty() == true);
+
+        StackLstT<T> st3;
+        StackLstT<T> st4 {5, 4, 3};
+        st4 = st3;
+        CHECK(st3.empty() == true);
+        CHECK(st4.empty() == true);
+        CHECK(st3.size() == 0);
+        CHECK(st4.size() == 0);
+    }
+    SUBCASE("Assignment with move") {
+        StackLstT<T> st1 {5, 4, 3};
+        StackLstT<T> st2;
+        st2 = std::move(st1);
+        CHECK(st1.empty() == true);
+        CHECK(st2.empty() == false);
+        CHECK(st2.size() == 3);
+        CHECK(st2.top() == 3);
+        st2.pop();
+        CHECK(st2.top() == 4);
+        CHECK(st2.size() == 2);
+        CHECK(st1.size() == 0);
+        st2.pop();
+        st2.pop();
+        CHECK(st1.empty() == true);
+        CHECK(st2.empty() == true);
+
+        StackLstT<T> st3;
+        StackLstT<T> st4 {5, 4, 3};
+        st4 = std::move(st3);
+        CHECK(st3.empty() == false);
+        CHECK(st4.empty() == true);
+        CHECK(st3.size() == 3);
+        CHECK(st4.size() == 0);
+    }
+}
+
+
+
+
+
