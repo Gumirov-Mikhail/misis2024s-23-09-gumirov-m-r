b5d9bd2f633ca2b4dee4b65838ce9a4a96c50277
Gumirov-Mikhail <m2309426@edu.misis.ru>
2024-04-18 04:06:35 +0300

    upd: queue

---
+++ prj.lab/queuearr/queuearr.cpp
@@ -0,0 +1,1 @@
+#include <queuearr/queuearr.hpp>
---
+++ prj.lab/queuelst/queuelst.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#ifndef QUEUELST_QUEUELST_HPP_202404
+#define QUEUELST_QUEUELST_HPP_202404
+
+#include <complex/complex.hpp>
+
+#include <cstddef>
+
+class QueueLst final {
+public:
+    QueueLst() = default;
+
+    QueueLst(const QueueLst& src);
+
+    QueueLst(QueueLst&& src) noexcept;
+
+    ~QueueLst() = default;
+
+    QueueLst& operator=(const QueueLst& src);
+
+    QueueLst& operator=(QueueLst&& src) noexcept;
+
+    [[nodiscard]] bool IsEmpty() const noexcept;
+
+    void Pop() noexcept;
+
+    void Push(const Complex& val);
+
+    [[nodiscard]] Complex& Top() &;
+
+    [[nodiscard]] const Complex& Top() const &;
+
+    void Clear() noexcept;
+
+private:
+    struct Node {
+        Complex val;
+        Node* next = nullptr;
+    };
+
+    Node* head_ = nullptr;   //!<
+    Node* tail_ = nullptr;   //!<
+};
+
+#endif
