8e2f4f32985ca8862b53fbadf92ebc070ba14717
Gumirov-Mikhail <m2309426@edu.misis.ru>
2024-04-28 00:36:57 +0300

    upd: queuearr

--- prj.lab/queuearr/queuearr.cpp
+++ prj.lab/queuearr/queuearr.cpp
@@ -9,6 +9,9 @@ QueueArr::QueueArr(const QueueArr& src) {
 
 QueueArr::QueueArr(QueueArr&& src) noexcept { //swaps
     std::swap(size_, src.size_);
+    std::swap(data_, src.data_);
+    std::swap(head_, src.head_);
+    std::swap(tail_, src.tail_);
 }
 
 QueueArr::~QueueArr() {
@@ -20,14 +23,17 @@ QueueArr& QueueArr::operator=(const QueueArr& src) {
 }
 
 QueueArr& QueueArr::operator=(QueueArr&& src) noexcept {
-
+    if (this != &src) {
+        std::swap(size_, src.size_);
+        std::swap(data_, src.data_);
+        std::swap(head_, src.head_);
+        std::swap(tail_, src.tail_);
+    }
+    return *this;
 }
 
 bool QueueArr::IsEmpty() const noexcept {
-    if (tail_ == -1) {
-        return true;
-    }
-    return false;
+    return head_ == -1;
 }
 
 void QueueArr::Pop() noexcept {
@@ -35,23 +41,39 @@ void QueueArr::Pop() noexcept {
 }
 
 void QueueArr::Push(const Complex& val) {
-
+    if (size_ == 0) {
+        size_ = 8;
+        data_ = new Complex* [size_];
+        head_++;
+    }
+    else if (tail_ + 1 == size_) {
+        Complex* temp = nullptr;
+        for (int i = 0; i < size_; i++) {
+            temp[i] = data_[i];
+        }
+    }
+    tail_++;
+    data_[tail_] = val;
 }
 
 Complex& QueueArr::Top() {
-
+    if (IsEmpty()) {
+        throw std::logic_error("QueueArr - try get top from empty queue");
+    }
+    return data_[head_];
 }
 
 const Complex& QueueArr::Top() const {
-
+    if (IsEmpty()) {
+        throw std::logic_error("QueueArr - try get top from empty queue");
+    }
+    return data_[head_];
 }
 
 void QueueArr::Clear() noexcept {
-    while(!IsEmpty()) {
-        Pop();
-    }
+    head_ == -1;
 }
 
 std::ptrdiff_t QueueArr::Count() const {
-
+    return size_;
 }
